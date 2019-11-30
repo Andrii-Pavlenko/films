d937018240e18398365062e6468784a8f20650f2
Andrii <farikpite@gmail.com>
2019-11-30 18:27:09 +0200

    Updates

---
+++ precache-manifest.b2bad0212b9aebfef62170bc3f872f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b90b7f6b8f7034bad75e37623af1b3",
+    "url": "/films/index.html"
+  },
+  {
+    "revision": "4e213d335bf4a2d82e82",
+    "url": "/films/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7ae2e261f11a9c6dacae",
+    "url": "/films/static/js/2.5b3771b0.chunk.js"
+  },
+  {
+    "revision": "4e213d335bf4a2d82e82",
+    "url": "/films/static/js/main.0ff4d533.chunk.js"
+  },
+  {
+    "revision": "a8a2b2e5ed01d881bc2a",
+    "url": "/films/static/js/runtime-main.ac8c4f00.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/films/static/media/logo.25bf045c.svg"
+  }
+]);
